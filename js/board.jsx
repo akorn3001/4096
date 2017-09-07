9f7b9aaf7cc54ca6bff9186781769d8346dc2fa3
akorn3001 <akorn3000@gmail.com>
2017-09-07 09:39:08 -0400

    Add event listener for keydown

--- js/board.jsx
+++ js/board.jsx
@@ -2,7 +2,10 @@ class Board {
   constructor() {
     this.grid = this.generateGrid();
   }
-
+// LEFT: 37
+// RIGHT: 39
+// UP: 38
+// DOWN: 40
   generateGrid () {
     let grid = [
         [0, 0, 0, 0],
@@ -11,20 +14,20 @@ class Board {
         [0, 0, 0, 0]
       ];
 
-    this.addTile(grid);
-    this.addTile(grid);
+    this.addTile(Math.floor(Math.random() * 4), Math.floor(Math.random() * 4), grid);
+    this.addTile(Math.floor(Math.random() * 4), Math.floor(Math.random() * 4), grid);
+
+    document.addEventListener('keydown', (event) => {
+      console.log(event.keyCode);
+      
+    });
 
     return grid;
   }
 
-  addTile(grid) {
-    let xCoord = Math.floor(Math.random() * 4);
-    let yCoord = Math.floor(Math.random() * 4);
-
-    grid[xCoord][yCoord] = 2;
+  addTile(x, y, grid) {
+    grid[x][y] = this.twoOrFour();
   }
-
-
 
   lost() {
     return !(this.grid[0].includes(0) ||
@@ -33,18 +36,30 @@ class Board {
     this.grid[3].includes(0));
   }
 
+  twoOrFour() {
+    return (Math.floor(Math.random() * 10) <= 2 ? 4 : 2);
+  }
+
   spawn() {
     let xCoord = Math.floor(Math.random() * 4);
     let yCoord = Math.floor(Math.random() * 4);
-    
-    if (!this.lost()) {
 
+    if (!this.lost()) {
       if (this.grid[xCoord][yCoord] === 0) {
-        this.addTile(this.grid);
+        this.addTile(xCoord, yCoord, this.grid);
         return this.grid;
       } else {
-        this.spawn();
+        return this.spawn();
       }
     }
+  }
+
+  render() {
+    this.grid.forEach((row, idx1) => {
+      row.forEach((el, idx2) => {
+        document.getElementById(`${idx1}-${idx2}`).innerHTML = el;
+      });
+    });
+
   }
 }
